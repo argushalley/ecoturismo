13f8bb3c0aa39d6d525ead470c3d05bebe1d723c
Argus Halley da Rocha <argus.pesquisa@gmail.com>
2015-03-14 12:47:19 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecoturismo::Application.config.secret_key_base = '7fc6724f9b335bcdaf0e0f87322a6f1925c5ae9f2305ecef30b59ba167b8a3895ed6656828425a6c52ec314452f4289071365f03f0d79da59713d4a94e6c59c8'

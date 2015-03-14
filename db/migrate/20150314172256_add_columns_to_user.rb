778f790c95bcee8584e20cd2f6265b77ec919131
Argus Halley da Rocha <argus.pesquisa@gmail.com>
2015-03-14 14:39:26 -0300

    Added fields to user and created user_data

---
+++ db/migrate/20150314172256_add_columns_to_user.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUser < ActiveRecord::Migration
+  def change
+    change_table :users do |t|
+      t.string  :username
+    end
+
+    add_index :users, :username, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314161513) do
+ActiveRecord::Schema.define(version: 20150314172321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "user_data", force: true do |t|
+    t.string   "name"
+    t.string   "cpf"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false

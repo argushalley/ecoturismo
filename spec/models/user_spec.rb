70963c000f92b62ec4b493afdbe6f802094b8b02
Argus Halley da Rocha <argus.pesquisa@gmail.com>
2015-03-23 11:41:16 -0300

    Refactored delegator matcher and added it to user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User, type: :model do
   it { is_expected.to have_many(:transactions) }
   it { is_expected.to have_one(:data).class_name('UserData').dependent(:destroy) }
   it { is_expected.to accept_nested_attributes_for(:data) }
-  #it { is_expected.to delegate(:name).to(:data).with_prefix }
+  it { is_expected.to delegate(:name).to(:data).with_prefix }
   it { is_expected.to define_enum_for(:role).with([:default, :admin]) }
   it { is_expected.to monetize(:balance_cents) }
 end
--- spec/support/matchers/delegate.rb
+++ spec/support/matchers/delegate.rb
@@ -1,25 +1,27 @@
 RSpec::Matchers.define :delegate do |method|
   match do |delegator|
     @method = @prefix ? :"#{@to}_#{method}" : method
+    param = @method =~ /=^/ ? 1 : nil
     @delegator = delegator
     begin
       @delegator.send(@to)
     rescue NoMethodError
       raise "#{@delegator} does not respond to #{@to}!"
     end
-    @delegator.stub(@to).and_return double('receiver')
-    @delegator.send(@to).stub(method).and_return :called
-    @delegator.send(@method) == :called
+    allow(@delegator).to receive(@to).and_return(double 'receiver')
+    allow(@delegator.send(@to)).to receive(method).with(param).and_return :called
+    @delegator.send(@method, param) == param || :called
   end
+
   description do
     "delegate :#{@method} to its #{@to}#{@prefix ? ' with prefix' : ''}"
   end
 
-  failure_message_for_should do |text|
+  failure_message do |text|
     "expected #{@delegator} to delegate :#{@method} to its #{@to}#{@prefix ? ' with prefix' : ''}"
   end
 
-  failure_message_for_should_not do |text|
+  failure_message_when_negated do |text|
     "expected #{@delegator} not to delegate :#{@method} to its #{@to}#{@prefix ? ' with prefix' : ''}"
   end
 

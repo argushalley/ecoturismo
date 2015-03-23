a22f5fd0a4e107107ba91578d0f909619bb7a6d7
Argus Halley da Rocha <argus.pesquisa@gmail.com>
2015-03-23 17:49:12 -0300

    Improved specs

--- spec/models/user_data_spec.rb
+++ spec/models/user_data_spec.rb
@@ -8,15 +8,15 @@ describe UserData, type: :model do
   it { is_expected.to allow_value(CPF.generate).for(:cpf) }
   it { is_expected.not_to allow_value("86402a6085b").for(:cpf) }
 
-  #it 'validates uniqueness of cpf' do
-  #  create(:user_data)
-  #  validate_uniqueness_of(:cpf)
-  #end
+  it 'validates uniqueness of cpf' do
+    create(:user_data)
+    validate_uniqueness_of(:cpf)
+  end
 
-  #context 'when rg is present' do
-  #  it 'validates uniqueness of rg' do
-  #    create(:user_data)
-  #    validate_uniqueness_of(:rg)
-  #  end
-  #end
+  context 'when rg is present' do
+    it 'validates uniqueness of rg' do
+      create(:user_data)
+      validate_uniqueness_of(:rg)
+    end
+  end
 end

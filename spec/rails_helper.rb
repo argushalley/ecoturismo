412b981f8584cfb9927f38cdcef3ff533cbab54d
Argus Halley da Rocha <argus.pesquisa@gmail.com>
2015-03-23 10:33:57 -0300

    Added user spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,8 @@ require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'shoulda/matchers'
+require 'money-rails/test_helpers'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -47,4 +49,6 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.include MoneyRails::TestHelpers, type: :model
 end

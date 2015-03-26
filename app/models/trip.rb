548f0aa4e598dae52b4230add285d3996b873886
Argus Halley da Rocha <argus.pesquisa@gmail.com>
2015-03-25 22:19:28 -0300

    Fixed transaction and trip validations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,9 +2,11 @@ class Trip < ActiveRecord::Base
   has_many :transactions, before_add: :build_transaction, dependent: :destroy
   has_many :users, through: :transactions
 
+  validates_presence_of :description, :users
+
   monetize :value_cents
 
   def build_transaction(transaction)
-    transaction.attributes= { type: :debit, compensated: true, value: self.value }
+    transaction.attributes= { description: self.description, type: :debit, compensated: true, value: self.value }
   end
 end

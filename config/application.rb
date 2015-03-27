5141c78375f7e78ccafed13b5bf9be17cc53a663
Argus Halley da Rocha <argus.pesquisa@gmail.com>
2015-03-27 09:55:13 -0300

    Added prawn gem and its configs to generate pdf reports

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module Ecoturismo
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false
+    config.autoload_paths << "#{Rails.root}/app/pdfs"
   end
 end

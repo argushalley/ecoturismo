787b6550c219dbb969f39c5a974a945631845b37
Argus Halley da Rocha <argus.pesquisa@gmail.com>
2015-03-28 02:39:57 -0300

    Fixed Trip associations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,12 +1,8 @@
 class Trip < ActiveRecord::Base
-  has_many :transactions, before_add: :build_transaction, dependent: :destroy
-  has_many :users, through: :transactions
+  has_many :transactions, dependent: :destroy
+  has_many :users, class_name: UserTrip
 
   validates_presence_of :description, :users
 
   monetize :value_cents
-
-  def build_transaction(transaction)
-    transaction.attributes= { description: self.description, type: :debit, compensated: true, value: self.value }
-  end
 end
